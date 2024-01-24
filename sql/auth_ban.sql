2609391f997dd85d3ec8aacb90e747c56366fc2d
s3min141 <minse0204@naver.com>
2024-01-24 20:39:52 +0000

    sql 파일 업데이트

--- sql/auth_ban.sql
+++ sql/auth_ban.sql
@@ -2,5 +2,6 @@ CREATE TABLE `auth_ban` (
 	`discord_uid` VARCHAR(50) NOT NULL COLLATE 'utf8mb4_general_ci',
 	`start_date` DATETIME NOT NULL,
 	`end_date` DATETIME NOT NULL,
-	`ban_reason` VARCHAR(50) NOT NULL COLLATE 'utf8mb4_general_ci'
+	`ban_reason` VARCHAR(50) NOT NULL COLLATE 'utf8mb4_general_ci',
+	PRIMARY KEY (`discord_uid`) USING BTREE
 ) COLLATE='utf8mb4_general_ci' ENGINE=InnoDB;
--- sql/verified_user.sql
+++ sql/verified_user.sql
@@ -2,5 +2,10 @@ CREATE TABLE `verified_user` (
 	`discord_uid` VARCHAR(50) NOT NULL COLLATE 'utf8mb4_general_ci',
 	`ubisoft_uid` VARCHAR(50) NOT NULL COLLATE 'utf8mb4_general_ci',
 	`ubisoft_uname` VARCHAR(50) NOT NULL COLLATE 'utf8mb4_general_ci',
-	`current_mmr` INT(10) NOT NULL
+	`current_mmr` INT(10) NOT NULL,
+	`current_kills` INT(10) NOT NULL,
+	`current_wins` INT(10) NOT NULL,
+	PRIMARY KEY (`discord_uid`) USING BTREE,
+	UNIQUE INDEX `ubisoft_uid` (`ubisoft_uid`) USING BTREE,
+	UNIQUE INDEX `ubisoft_uname` (`ubisoft_uname`) USING BTREE
 ) COLLATE='utf8mb4_general_ci' ENGINE=InnoDB;
